d557a84598034696808e14ff68611b368302623a
snhuhcfaith <hope.faith@snhu.edu>
2026-03-09 23:48:40 -0400

    I just want a nice looking background :(

--- assets/scripts/box.gd
+++ assets/scripts/box.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet"):
-		box_sprite.texture = preload("res://assets/art/watercolor-flower-garden-clipart-free-png.png")
+		box_sprite.texture = preload("res://assets/art/2d/items/watercolor-flower-garden-clipart-free-png.png")
 		box_collision.disabled = true
 		collision_layer = 0   
 		collision_mask = 0   
